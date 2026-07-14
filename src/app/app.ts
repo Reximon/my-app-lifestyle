685a72fc22ec7a0a6661b22f41253acbe89af193
Reximon <reximon161@gmail.com>
2026-07-14 21:22:25 +0200

    build: agregar Font Awesome como librería de iconos
    npm: @fortawesome/angular-fontawesome@4, fontawesome-svg-core, free-solid/regular/brands-svg-icons - app.ts: configurar FaIconLibrary con iconos comunes (check, plus, play, pause, trash, save, etc.) - build: 368 kB, 0 vulnerabilidades

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faCheck, faPlus, faPlay, faPause, faRotate, faChevronLeft, faChevronRight, faTrash, faSave, faTimes, faFilter, faCalendar, faClock } from '@fortawesome/free-solid-svg-icons';
 import { Clock } from "./components/clock/clock";
 import { ActionsPanel } from './components/actions-panel/actions-panel';
 import { TodoWeek } from './components/todo-week/todo-week';
@@ -8,10 +10,14 @@ import { CalendarView } from './components/calendar-view/calendar-view';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Clock, ActionsPanel, TodoWeek, Pomodoro,CalendarView],
+  imports: [RouterOutlet, Clock, ActionsPanel, TodoWeek, Pomodoro, CalendarView],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('my-academic-os');
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faCheck, faPlus, faPlay, faPause, faRotate, faChevronLeft, faChevronRight, faTrash, faSave, faTimes, faFilter, faCalendar, faClock);
+  }
 }
